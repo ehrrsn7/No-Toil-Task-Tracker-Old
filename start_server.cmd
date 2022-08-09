eaebe7a23779be939b4e3b3def4b4841749a847f
E Harrison <har16020@byui.edu>
2022-08-09 15:58:09 -0700

    remove some stuff

--- start_server.cmd
+++
@@ -1,5 +0,0 @@
-@REM start server
-@REM getting ip address for this machine...
-for /f "tokens=1-2 delims=:" %%a in ('ipconfig^|find "IPv4"') do set ip=%%b
-set ip=%ip:~1%
-python manage.py runserver %ip%:8000 
