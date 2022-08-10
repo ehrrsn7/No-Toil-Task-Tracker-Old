3dca16b3136cff1b42a1a6d5fc1d826700f97151
ehrrsn7 <har16020@byui.edu>
2022-08-10 11:07:41 -0700

    updated and polished runserver arguments logic

--- runserver.py
+++ runserver.py
@@ -11,6 +11,7 @@ def get_ip_address():
       s.connect(('10.255.255.255', 1))
       ip_address = s.getsockname()[0]
    except Exception:
+      print(Exception, "unable to get ip address, defaulting to localhost")
       ip_address = '127.0.0.1'
    finally:
       s.close()
@@ -34,26 +35,42 @@ class Unix:
 def main():
    cwd = os.getcwd()
 
+   # no matter what, we want to activate our venv
+   result = os.system(".\\Scripts\\activate")
+   if result != 0: 
+      print("Virtual environment not activated!")
+      print("Be sure to initialize it after instal by running the command")
+      print("python -m venv ./")
+      return
+
    # python runserver.py update
    if len(sys.argv) > 1:
       if sys.argv[1].lower() == "update":
+
          # update latest file changes from developer
          os.system(f"git pull")
          # update python packages
          os.system(f"pip install --upgrade pip")
          os.system(f"pip install -r requirements.txt")
-         # change dir to djangoapp and update npm packages
-         os.system("cd djangoapp")
-         os.chdir(cwd + "/djangoapp")
-         os.system(f"npm install npm@latest")
-         os.system(f"npm install --legacy-peer-deps")
-         os.system(f"npm outdated")
-         os.system(f"cd ..")
+         # now that django is updated, update django project migrations
+         os.system(f"python manage.py makemigrations")
+         os.system(f"python manage.py migrate")
+
+         # python runserver.py update react
+         if len(sys.argv) > 2:
+            if sys.argv[1].lower() == "react":
+
+               # change dir to djangoapp and update npm packages
+               os.system("cd djangoapp")
+               os.chdir(cwd + "/djangoapp")
+               os.system(f"npm install npm@latest")
+               os.system(f"npm install --legacy-peer-deps")
+               os.system(f"npm outdated")
+               os.system(f"cd ..")
    
    # python runserver.py (default)
-   elif platform.system() == "Darwin" or platform.system() == "Linux":
+   elif platform.system() in ["Darwin", "Linux"]:
       Unix.runserver()
    else: Windows.runserver()
-
 
 if __name__ == "__main__": main()
