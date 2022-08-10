a88796e8b8a91fc02029ae96a7027b8fd1ca6bdc
ehrrsn7 <har16020@byui.edu>
2022-08-09 17:07:39 -0700

    some more

--- djangoapp/src/pages/CompletedParts.jsx
+++ djangoapp/src/pages/CompletedParts.jsx
@@ -1,32 +1,26 @@
-import React from "react"
-import * as Buttons from "../components/buttons"
-import { documentTitleSuffix } from "../App"
-import { useContext } from "../contexts/contextProvider"
-import { CompletedPartsTable } from "../components/tables"
+import React                    from "react"
+import { documentTitleSuffix }  from "../App"
+import { useContext }           from "../contexts/contextProvider"
+import * as Buttons             from "../components/buttons"
+import { CompletedPartsTable }  from "../components/tables"
 
 export default function CompletedParts() {
-    const context = useContext()
+   const context = useContext()
 
-    React.useEffect(() => {
-        const currentDateTime = ' ' + new Date().toLocaleString()
-        document.title = "Completed Parts"
-        document.querySelector("#headerTitle").innerText = document.title
-        document.querySelector("title").textContent =
+   React.useEffect(() => {
+      const currentDateTime = ' ' + new Date().toLocaleString()
+      document.title = "Completed Parts"
+      document.querySelector("#headerTitle").innerText = document.title
+      document.querySelector("title").textContent =
+         document.title + documentTitleSuffix + currentDateTime
+   }, [])
 
-            document.title + documentTitleSuffix + currentDateTime
-    }, [])
-
-    return <div id = { document.title.replace(' ', '') } >
-        <
-        span >
-        <
-        Buttons.BackToDashboardButton / >
-
-        <
-        Buttons.PrintCompletedPartsButton / >
-        <
-        /span> <
-        CompletedPartsTable context = { context }
-    /> <
-    /div>
+   return <div id={ document.title.replace(' ', '') }>
+      <span>
+         <Buttons.BackToDashboardButton />
+         <Buttons.PrintCompletedPartsButton />
+      </span> 
+      
+      <CompletedPartsTable context={ context } /> 
+   </div>
 }
