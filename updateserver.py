29d57bc0edbaca999c0883c6318116b588b2b28c
ehrrsn7 <har16020@byui.edu>
2022-08-10 11:55:52 -0700

    ?sdkfnwe

--- updateserver.py
+++ updateserver.py
@@ -1,3 +1,10 @@
-import runserver
-runserver.main()
-input("press any key to exit.")
+if __name__ == "__main__":
+   import os
+   os.system(f"git pull")
+   # update python packages
+   os.system(f"pip install --upgrade pip")
+   os.system(f"pip install -r requirements.txt")
+   # now that django is updated, update django project migrations
+   os.system(f"python manage.py makemigrations")
+   os.system(f"python manage.py migrate")
+   input("press any key to exit.")
