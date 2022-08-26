2b98f38db037d79517a913bf5f63b5ace120db6c
ehrrsn7 <har16020@byui.edu>
2022-08-26 14:06:03 -0700

    wekfljsdlf

--- manage.py
+++ manage.py
@@ -13,6 +13,7 @@ def main(kwargs):
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        print(exc)
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
