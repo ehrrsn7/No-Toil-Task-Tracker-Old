f4723f62a49f073b73ab56cf8b0d7152def1122c
E Harrison <har16020@byui.edu>
2022-08-26 11:46:52 -0700

    included exception handling into updateserver to make sure things run more smoothly

--- updateserver.py
+++ updateserver.py
@@ -1,17 +1,28 @@
 import os, sys, manage
 
 if __name__ == "__main__": 
-   # pull all code changes
-   os.system(f"git pull")
-   # update python packages
-   os.system(f"pip install --upgrade pip")
-   os.system(f"pip install -r requirements.txt --no-warn-script-location")
-   # now that django is updated, reset/update django project migrations
-   # manage.main([sys.argv[0], "migrate", "api", "zero"])  # reset
-   manage.main([sys.argv[0], "makemigrations"])          # update
-   manage.main([sys.argv[0], "migrate"])                 # execute update
+   try:
+      # pull all code changes
+      os.system(f"git pull")
+   except:
+      print(">> GIT ERROR")
 
-   if len(sys.argv) > 1:
+   try:
+      # update python packages
+      os.system(f"pip install --upgrade pip")
+      os.system(f"pip install -r requirements.txt --no-warn-script-location")
+   except:
+      print(">> PIP ERROR")
+
+   try:
+      # now that django is updated, reset/update django project migrations
+      # manage.main([sys.argv[0], "migrate", "api", "zero"])  # reset
+      manage.main([sys.argv[0], "makemigrations"])          # update
+      manage.main([sys.argv[0], "migrate"])                 # execute update
+   except:
+      print(">> DJANGO ERROR")
+
+   if sys.argv.get(1) == "npm":
       # change dir to djangoapp and update npm packages
       os.system("cd djangoapp")
       os.chdir(cwd + "/djangoapp")
